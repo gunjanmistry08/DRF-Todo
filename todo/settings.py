6b43aec0e7644c19625284ec74710a8fcb0e2562
Gunjan Mistry <gunjanmistry008@gmail.com>
2021-04-30 17:50:13 +0530

    add host

--- todo/settings.py
+++ todo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-&v5y$!=^4)!aox1rf^uwpmzq0%4cobbciwif71tpzto4-kkw38
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["djangotodo123.herokuapp.com"]
 
 
 # Application definition
